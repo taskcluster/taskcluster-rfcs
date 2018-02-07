fedf294750bc01d2c1fe011a18ddc5c631665ce8
Dustin J. Mitchell <dustin@mozilla.com>
2018-02-07 00:54:57 +0000

    updated script

--- issue-to-pr/run.js
+++ issue-to-pr/run.js
@@ -3,9 +3,7 @@ const Github = require('github');
 const fs = require('mz/fs');
 const path = require('path');
 
-const github = new Github({
-  Promise: Promise,
-});
+const github = new Github({});
 
 // We get more ratelimit capacity if we auth
 if (process.env.GITHUB_TOKEN) {
@@ -15,25 +13,15 @@ if (process.env.GITHUB_TOKEN) {
   });
 }
 
-const owner = 'taskcluster';
-const repo = 'taskcluster-rfcs';
+const owner = 'djmitche';
+const repo = 'testaroo';
 
-async function getCommentsForIssue({owner, repo, number}) {
-  let comments = [];
-  let res = await github.issues.getComments({
-    owner,
-    repo,
-    number,
-    //per_page: 100
-  });
-
-  Array.prototype.push.apply(comments, res.data);
-  while (github.hasNextPage(res)) {
-    res = await github.getNextPage(res);
-    Array.prototype.push.apply(comments, res.data);
-  }
-
-  return comments;
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
 }
 
 async function main() {
@@ -45,13 +33,21 @@ async function main() {
     per_page: 100,
 
   });
+
   Array.prototype.push.apply(issues, res.data);
   while (github.hasNextPage(res)) {
     res = await github.getNextPage(res);
     Array.prototype.push.apply(issues, res.data);
   }
 
-  for (let {number, id, title, created_at, updated_at, body, url, labels} of issues) {
+  let master = await github.gitdata.getReference({owner, repo, ref: 'heads/master'});
+  master = await github.gitdata.getCommit({owner, repo, sha: master.data.object.sha});
+
+  for (let {pull_request, number, title, created_at, updated_at, body, url, labels, state, user} of issues) {
+    // skip pull requests..
+    if (pull_request) {
+      continue;
+    }
     created_at = new Date(created_at);
     let year = '' + (created_at.getYear() + 1900);
     let month = created_at.getMonth() + 1;
@@ -59,69 +55,95 @@ async function main() {
     let day = created_at.getDate();
     day = day > 9 ? ''+day : '0'+day;
     let timestamp = [year, month, day].join('-');
-    let fixedtitle = title.trim();
-    for (let c of ['/', '\\\\', '?', '%', '*', ':', '|', '"', '<', '>']) {
-      let pattern = new RegExp('[' + c + ']', 'g');
-      fixedtitle = fixedtitle.replace(pattern, '_');
-    }
-    let filename = `${timestamp} ${fixedtitle}.md`
-    let comments = await getCommentsForIssue({owner, repo, number});
+    let fixedtitle = title.trim().replace(/[^a-zA-Z0-9]+/g, '-');
 
     labels = labels.map(x => x.name);
 
-    let state;
+    console.log(`working on ${title}`);
+
+    let phase;
     if (labels.includes('Phase: Decided')) {
-      state = 'decided';
+      phase = 'decided';
     } else if (labels.includes('Phase: Proposal')) {
-      state = 'proposal';
+      phase = 'proposal';
     } else if (labels.includes('Phase: Final Comment')) {
-      state = 'final-comment';
+      phase = 'final-comment';
+    } else {
+      console.log('skipping - not labeled');
+      continue;
     }
 
+    if (state !== 'open' && phase !== 'decided') {
+      console.log('skipping - closed but not decided');
+      continue;
+    }
 
-    // NOTE We do not apply any attribution to the body of the issue because
-    // it is considered a shared space
+    let filename = `rfcs/${pad(number, 4)}-${fixedtitle}.md`;
     let document = [
-      `RFC: ${timestamp} ${title}`,
-      '================================================================================',
-      `**NOTE**: This RFC was converted from an [issue](${url}) to a PR RFC.`,
+      `# RFC ${number} - ${title}`,
+      `* Comments: [#${number}](${url})`,
+      `* Initially Proposed by: @${user.login}`,
       '',
-      '',
-      'SUMMARY',
-      '--------------------------------------------------------------------------------',
+      body.startsWith('#') ? '' : '# Proposal',
       body.replace(/[\r]/g, ''),
-      '',
-      '',
-      'DISCUSSION',
-      '--------------------------------------------------------------------------------',
     ].join('\n');
-    for (let {body, url, user, updated_at} of comments) {
-      document += [
-        '',
-        `[Comment](${url}) [${user.login}](${user.html_url}) ${updated_at}`,
-        '--------------------------------------------------------------------------------',
-        body.replace(/[\r]/g, ''),
-        '',
-        '',
-      ].join('\n');
+
+    let blob = await github.gitdata.createBlob({
+      owner, repo,
+      content: document,
+      encoding: 'utf-8',
+    });
+
+    let tree = await github.gitdata.createTree({
+      owner, repo,
+      tree: [
+        {path: filename, mode: "100644", type: "blob", sha: blob.data.sha},
+      ],
+      base_tree: master.data.tree.sha,
+    });
+
+    let commit = await github.gitdata.createCommit({
+      owner, repo,
+      message: `Add RFC #${number}`,
+      tree: tree.data.sha,
+      parents: [master.data.sha],
+    });
+
+    if (state !== 'open') {
+      // for closed issues, add to master and comment on the
+      // issue
+
+      let ref = await github.gitdata.updateReference({
+        owner, repo,
+        ref: 'heads/master',
+        sha: commit.data.sha,
+      });
+
+      // ..and we have a new master
+      master = commit;
+
+      await github.issues.createComment({
+        owner, repo, number,
+        body: `This RFC is stored as [${filename}](https://github.com/${owner}/${repo}/blob/master/${filename})`,
+      });
+      console.log('committed to master and commented on the issue');
+    } else {
+      // for open issues, create a PR
+      let ref = await github.gitdata.createReference({
+        owner, repo,
+        ref: `refs/heads/rfc${number}`,
+        sha: commit.data.sha,
+      });
+
+      let pr = await github.pullRequests.createFromIssue({
+        owner, repo,
+        issue: number,
+        head: `rfc${number}`,
+        base: 'master',
+      });
+      console.log('converted issue to pull request');
     }
-
-    await fs.writeFile(path.join('..', 'rfcs', filename), document);
-    console.log('Wrote: ' + filename);
-
-    /*
-     * What we should consider:
-     * 
-     *  1. Make the issue body (e.g. first comment) the sole contents of the file
-     *  2. Create a git branch which adds the file and push it
-     *  3. Open a PR using the same title
-     *  4. Mark the PR with the same labels that the issue had
-     *  5. Instead of adding comments to the file, make a comment in the PR with
-     *     attribution and a link to the original comment
-     *  6. Close the github issue corresponding to the new PR with a comment redirecting
-     *     the reader
-     */
   }
 }
 
-main().then(console.log, console.error);
+main().catch(console.error);
